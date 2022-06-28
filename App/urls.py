bd01c3f481181b0abdb4cd338942e28c2847011d
PabloDiaz1990 <pablo.diaz.g@hotmail.es>
2022-06-28 01:22:24 -0400

    Ingresar y buscar para Cursos

--- App/urls.py
+++ App/urls.py
@@ -4,6 +4,9 @@ from App import views
 urlpatterns = [
     path('', views.inicio, name="Inicio"),
     path('cursos', views.cursos, name="Cursos"),
+    path('cursoForm', views.curso_formulario, name="CursoFormulario"),
+    path('cursoBusqueda', views.curso_busqueda, name="CursoBusqueda"),
+    path('cursoBuscar', views.curso_buscar),
     path('estudiantes', views.estudiantes, name="Estudiantes"),
     path('profesores', views.profesores, name="Profesores")
 ]
--- App/views.py
+++ App/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from App.models import *
+from App.forms import CursoFormulario
 
 def inicio(request):
     return render(request, "App/inicio.html")
 
 def cursos(request):
     return render(request, "App/cursos.html")
+
+def curso_formulario(request):
+    if request.method == 'POST':
+        form = CursoFormulario(request.POST)
+        print(form)
+        
+        if form.is_valid:
+            info = form.cleaned_data
+            curso = Curso(nombre=info['curso'], numero_curso=info['numero_curso'])
+            curso.save()
+
+            return render(request, "App/cursos.html")
+    else:
+
+        form = CursoFormulario()
+    
+    return render(request, "App/curso_form.html", {"form":form})
+
+def curso_busqueda(request):
+    return render(request, "App/curso_form_buscar.html")
+
+def curso_buscar(request):
+    if request.GET['num_curso']:
+        #resp = f"Buscando el número de curso: {request.GET['num_curso']}"
+        num_curso = request.GET['num_curso']
+        curso = Curso.objects.filter(numero_curso__icontains=num_curso)
+
+        return render(request, "App/curso_resp_busqueda.html", {"curso":curso, "num_curso":num_curso})
+    
+    else:
+        resp = "Dato sin ingresar"
+
+    return HttpResponse(resp)
 
 def profesores(request):
     return render(request, "App/profesores.html")
